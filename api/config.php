310820183c1f2858541e4a56f32caaebc58b4de2
Rishisaran9585 <rishisaran7867@gmail.com>
2026-01-08 14:23:01 +0530

    l

--- api/config.php
+++ api/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database Configuration
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'kind_craft_portal');
+define('DB_USER', 'u891495087_kind_craft');
+define('DB_PASS', 'KindCraft8@');
+define('DB_NAME', 'u891495087_kind_craft');
 
 function getDBConnection() {
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
